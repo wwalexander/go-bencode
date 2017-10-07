6e7ff896af7479227f8017889cf59eaf88ed0fdc
Will Alexander <willwalexander@icloud.com>
2017-10-07 12:36:29 -0700

    Add 'encoding' to package description

--- bencode.go
+++ bencode.go
@@ -1,4 +1,4 @@
-// Package bencode implements decoding of bencoded values as defined in BEP 3.
-// The mapping between bencoded and Go values is described in the documentation
-// for the Marshal and Unmarshal functions.
+// Package bencode implements encoding and decoding of bencoded values as
+// defined in BEP 3. The mapping between bencoded and Go values is described in
+// the documentation for the Marshal and Unmarshal functions.
 package bencode
