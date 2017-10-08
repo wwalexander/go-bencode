7f19c8a6ee3d38a0edb61897a1472252d7d191f1
Will Alexander <willwalexander@icloud.com>
2017-10-07 21:26:07 -0700

    Add more json-like struct key behavior to Marshal

--- encode.go
+++ encode.go
@@ -8,6 +8,7 @@ import (
 	"reflect"
 	"sort"
 	"strconv"
+	"strings"
 )
 
 // An Encoder writes bencoded values to an output stream.
@@ -86,10 +87,32 @@ func (enc *Encoder) encode(v interface{}) (err error) {
 		names := make([]string, 0, val.NumField())
 		fields := make(map[string]int)
 		for i := 0; i < val.NumField(); i++ {
-			name, ok := typ.Field(i).Tag.Lookup("bencode")
-			if !ok {
-				continue
+			field := typ.Field(i)
+			name, ok := field.Tag.Lookup("bencode")
+			if ok {
+				if name == "-" {
+					continue
+				}
+				strs := strings.Split(name, ",")
+				name = strs[0]
+				if len(strs) > 1 {
+					opts := make(map[string]bool)
+					for _, s := range strs[1:] {
+						opts[s] = true
+					}
+					if _, ok := opts["omitempty"]; ok {
+						field := val.Field(i)
+						x := field.Interface()
+						y := reflect.Zero(field.Type()).Interface()
+						if reflect.DeepEqual(x, y) {
+							continue
+						}
+					}
+				}
+			} else {
+				name = field.Name
 			}
+
 			names = append(names, name)
 			fields[name] = i
 		}
@@ -129,13 +152,48 @@ func (enc *Encoder) Encode(v interface{}) error {
 //
 // int values encode as bencoded integers.
 //
+// String values encode as bencoded strings.
+//
 // Slice values encode as bencoded lists, except that []byte encodes as a
 // bencoded string.
 //
 // Struct values encode as bencoded dictionaries. Each exported struct field
-// with a "bencode" key in its tag becomes an entry in the dictionary, using the
-// key as the dictionary key. Dictionaries are encoded with their entries sorted
-// by their keys as raw strings.
+// becomes a member of the object, using the field name as the dictionary key,
+// unless the field is omitted for one of the reasons given below.
+//
+// The encoding of each struct field can be customized by the format string
+// stored under the "bencode" key in the struct field's tag. The format string
+// gives the name of the field, possibly followed by a comma-separated list of
+// options. The name may be empty in order to specify options without overriding
+// the default field name.
+//
+// The "omitempty" option specifies that the field should be omitted from the
+// encoding if the field has an empty value, defined as 0 and any empty slice or
+// string.
+//
+// As a special case, if the field tag is "-", the field is always omitted. Note
+// that a field with name "-" can still be generated using the tag "-,".
+//
+// Examples of struct field tags and their meanings:
+//
+//   // Field appears in bencoded as key "myName".
+//   Field int `bencode:"myName"`
+//
+//   // Field appears bencoded as key "myName" and
+//   // the field is omitted from the dictionary if its value is empty,
+//   // as defined above.
+//   Field int `bencode:"myName,omitempty"`
+//
+//   // Field appears bencoded as key "Field" (the default), but
+//   // the field is skipped if empty.
+//   // Note the leading comma.
+//   Field int `bencode:",omitempty"`
+//
+//   // Field is ignored by this package.
+//   Field int `bencode:"-"`
+//
+//   // Field appears bencoded as key "-".
+//   Field int `bencode:"-,"`
 func Marshal(v interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := NewEncoder(buf).Encode(v); err != nil {
